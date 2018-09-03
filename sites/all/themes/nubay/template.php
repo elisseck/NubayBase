0977f8ff366115dca4b9f2d123c3364c465ed3aa
Mark Hanna <mark@skvare.com>
2018-09-03 10:52:24 -0500

    adding civicrm theme extension, code cleanup and consolidation

--- sites/all/themes/nubay/template.php
+++ sites/all/themes/nubay/template.php
@@ -12,26 +12,29 @@
 
 
 /**
- * Override or insert variables for the html template.
- */
-/* -- Delete this line if you want to use this function
-function nubay_preprocess_html(&$vars) {
-}
-function nubay_process_html(&$vars) {
-}
-// */
-
-
-/**
  * Override or insert variables for the page templates.
  */
-/* -- Delete this line if you want to use these functions
 function nubay_preprocess_page(&$vars) {
+  // load CiviCRM Extension style library on every civicrm page
+  if (arg(0) == 'civicrm') {
+    if (module_exists('style_library_entity')) {
+      $civicrm_extension_enabled = theme_get_setting('nubay_civicrm_enable');
+      if (!empty($civicrm_extension_enabled)) {
+        $style_library_id = theme_get_setting('nubay_civicrm_style_library');
+        if (!empty($style_library_id)) {
+          try {
+            $style_library = entity_load_single('style_library_entity', $style_library_id);
+            style_library_entity_add_style_library_css_to_theme($style_library);
+            style_library_entity_add_style_library_js_to_theme($style_library);
+          }
+          catch (Exception $e) {
+            watchdog('nubay_theme_extension', $e->getMessage());
+          }
+        }
+      }
+    }
+  }
 }
-function nubay_process_page(&$vars) {
-}
-// */
-
 
 /**
  * Override or insert variables into the node templates.
@@ -41,18 +44,7 @@ function nubay_preprocess_node(&$vars) {
 }
 function nubay_process_node(&$vars) {
 }
-// */
-
-
-/**
- * Override or insert variables into the comment templates.
- */
-/* -- Delete this line if you want to use these functions
-function nubay_preprocess_comment(&$vars) {
-}
-function nubay_process_comment(&$vars) {
-}
-// */
+*/
 
 
 /**
@@ -115,103 +107,29 @@ function nubay_form_alter(&$form, &$form_state, $form_id) {
  * @param $vars
  */
 function nubay_preprocess_html(&$vars) {
-
   global $theme_key;
   $theme_name = $theme_key;
   if (at_get_setting('enable_extensions', $theme_name) == 1) {
     // Get the path to the directory where our CSS files are saved
     $path = variable_get('theme_' . $theme_name . '_files_directory');
-
-    // Load the themestyles CSS from public files
-    $filepath = $path . '/' . $theme_name . '.themestyles.css';
-    if (file_exists($filepath)) {
-      drupal_add_css($filepath, array(
-          'preprocess' => TRUE,
-          'group' => CSS_THEME,
-          'media' => 'screen',
-          'every_page' => TRUE,
-        )
-      );
-    }
-    else {
-      at_load_failure($filepath, $theme_name);
-    }
-
-    // load the inlineblock styles
-    $filepath = $path . '/' . $theme_name . '.inlineblock-styles.css';
-    if (file_exists($filepath)) {
-      drupal_add_css($filepath, array(
-          'preprocess' => TRUE,
-          'group' => CSS_THEME,
-          'media' => 'screen',
-          'every_page' => TRUE,
-        )
-      );
-    }
-    else {
-      at_load_failure($filepath, $theme_name);
-    }
-
-    // load the Block Margins styles
-    $filepath = $path . '/' . $theme_name . '.blockmargins-styles.css';
-    if (file_exists($filepath)) {
-      drupal_add_css($filepath, array(
-          'preprocess' => TRUE,
-          'group' => CSS_THEME,
-          'media' => 'screen',
-          'every_page' => TRUE,
-        )
-      );
-    }
-    else {
-      at_load_failure($filepath, $theme_name);
-    }
-
-    // load menu/footer regions inline styles
-    $filepath = $path . '/' . $theme_name . '.inlineregions-styles.css';
-    if (file_exists($filepath)) {
-      drupal_add_css($filepath, array(
-          'preprocess' => TRUE,
-          'group' => CSS_THEME,
-          'media' => 'screen',
-          'every_page' => TRUE,
-        )
-      );
-    }
-    else {
-      at_load_failure($filepath, $theme_name);
-    }
-
-    // superfish menu styles
-    $filepath = $path . '/' . $theme_name . '.superfish-styles.css';
-    if (file_exists($filepath)) {
-      drupal_add_css($filepath, array(
-          'preprocess' => TRUE,
-          'group' => CSS_THEME,
-          'media' => 'screen',
-          'every_page' => TRUE,
-        )
-      );
-    }
-    else {
-      at_load_failure($filepath, $theme_name);
-    }
-
-    // webform styles
-    $filepath = $path . '/' . $theme_name . '.webform-styles.css';
-    if (file_exists($filepath)) {
-      drupal_add_css($filepath, array(
-          'preprocess' => TRUE,
-          'group' => CSS_THEME,
-          'media' => 'screen',
-          'every_page' => TRUE,
-        )
-      );
-    }
-    else {
-      at_load_failure($filepath, $theme_name);
+    $file_paths = [];
+    $file_paths[] = $path . '/' . $theme_name . '.themestyles.css';
+    $file_paths[] = $path . '/' . $theme_name . '.inlineblock-styles.css';
+    $file_paths[] = $path . '/' . $theme_name . '.blockmargins-styles.css';
+    $file_paths[] = $path . '/' . $theme_name . '.inlineregions-styles.css';
+    $file_paths[] = $path . '/' . $theme_name . '.superfish-styles.css';
+    $file_paths[] = $path . '/' . $theme_name . '.webform-styles.css';
+    $file_paths[] = $path . '/' . $theme_name . '.civicrm-styles.css';
+    foreach ($file_paths as $file_path) {
+      if (file_exists($file_path)) {
+        drupal_add_css($file_path, array(
+            'preprocess' => TRUE,
+            'group' => CSS_THEME,
+            'media' => 'screen',
+            'every_page' => TRUE,
+          )
+        );
+      }
     }
   }
-  
 }
-
